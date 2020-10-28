e927bfbd60c4990f3441fc1d6bd79120a6bfa979
adianbu <57458067+adianbu@users.noreply.github.com>
2020-10-28 14:33:36 +0530

    App functions properly

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+// import 'dart:js';
+
+import 'package:flutter/material.dart';
+import 'pages/home.dart';
+import 'package:worldtime/pages/loading.dart';
+import 'package:worldtime/pages/home.dart';
+import 'package:worldtime/pages/choose_location.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
+  ));
+}
